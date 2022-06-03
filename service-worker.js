f48125edf7e7dd76d4947653bcda4300b5632962
Stanislav Tebloev <stas.tebloev@yandex.ru>
2022-06-03 10:08:56 +0300

    Fix service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,6 +20,16 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+
   // Tell the active service worker to take control of the page immediately.
   self.clients.claim();
 });
@@ -27,14 +37,32 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
-      const cache = await caches.open(CACHE_NAME);
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
 
-      if (event.request.url === `${BASE_URL}/`) {
-        return await cache.match("/index.html");
-      }
-      for (url of URLS_TO_SAVE) {
-        if (event.request.url === `${BASE_URL}${url}`) {
-          return await cache.match(url);
+        // Always try the network first.
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log("Fetch failed; returning offline page instead.", error);
+
+        const cache = await caches.open(CACHE_NAME);
+
+        if (event.request.url === `${BASE_URL}/`) {
+          return await cache.match("/index.html");
+        }
+        for (url of URLS_TO_SAVE) {
+          if (event.request.url === `${BASE_URL}${url}`) {
+            return await cache.match(url);
+          }
         }
       }
     })()
