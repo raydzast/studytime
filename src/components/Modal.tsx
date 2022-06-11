3a66fa3c3285b694a909d9259e57cdb4c91248b8
Stanislav <stas.tebloev@yandex.ru>
2022-06-11 02:09:40 +0000

    Minor fixes

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,16 +4,20 @@ type Props = {
   show: boolean;
   children: React.ReactNode;
   onHide: React.MouseEventHandler<HTMLDivElement>;
+  hideOnBgClick?: boolean;
 };
 
-function Modal({ show, children, onHide }: Props) {
+function Modal({ show, children, onHide, hideOnBgClick }: Props) {
   if (!show) {
     return null;
   }
 
   return (
     <div className="modal-wrapper">
-      <div className="modal-background" onClick={onHide} />
+      <div
+        className="modal-background"
+        onClick={hideOnBgClick === true ? onHide : null}
+      />
       <div className="modal-container">{children}</div>
     </div>
   );
